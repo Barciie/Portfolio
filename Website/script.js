c0e34374e236caef27f7af1ba4e30e9199c2839b
BasetsanaAmelda <70413094+Barciie@users.noreply.github.com>
2024-06-14 12:28:24 +0200

    Delete Website directory

--- Website/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var skillBars = document.querySelectorAll('.skill-bar');
-    skillBars.forEach(function(skillBar) {
-        var percent = skillBar.getAttribute('data-percent');
-        skillBar.innerHTML = '<div class="skill-bar-fill" style="width: ' + percent + '%"></div>';
-    });
-  });
-  
